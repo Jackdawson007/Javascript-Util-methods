32b2ef3afd4424d75fce1547476faf49b5586845
Jackdawson007 <154601146+Jackdawson007@users.noreply.github.com>
2024-01-04 20:47:57 +0530

    Javascript_util_methods.js

--- Javascript_util_methods.js
+++ Javascript_util_methods.js
@@ -45,3 +45,6 @@ String.prototype.IndexAllString = function(findvalue) {
     return output_list;
 };
 
+String.prototype.equalsIgnoreCase = function(str1,str2) {
+	return str1.toLowerCase() === str2.toLowerCase();
+}
